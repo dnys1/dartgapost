1e2256355bfb0e668e35d8994ce152bf2f205fc0
Abdallah Shaban <abdallah.w.shaban@gmail.com>
2023-04-03 21:36:56 -0700

    updated main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:dartgapost/homepage.dart';
 import 'package:flutter/material.dart';
 //API feature imports
 import 'package:amplify_api/amplify_api.dart';
@@ -45,10 +46,11 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return Authenticator(
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         builder: Authenticator.builder(),
         home: const Scaffold(
           body: Center(
-            child: Text('You are logged in!'),
+            child: Homepage(),
           ),
         ),
       ),
