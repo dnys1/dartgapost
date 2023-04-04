16d10df7b2e3bfad4eed6f2e826ca05c211700cd
Abdallah Shaban <abdallah.w.shaban@gmail.com>
2023-04-03 21:03:52 -0700

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "dartgapost": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "dartgapost9bfba83e": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3c2e884f3": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
