291ccc7359129713218020808c17ef11fc0ecff8
Abdallah Shaban <abdallah.w.shaban@gmail.com>
2023-04-04 00:26:15 -0700

    updated the add/remove budgetEntries

--- lib/createbudgetentry.dart
+++ lib/createbudgetentry.dart
@@ -1,3 +1,6 @@
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:dartgapost/homepage.dart';
 import 'package:dartgapost/models/ModelProvider.dart';
 import 'package:flutter/material.dart';
 
@@ -12,9 +15,42 @@ class Createbudgetentry extends StatefulWidget {
 }
 
 class _CreatebudgetentryState extends State<Createbudgetentry> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _amountController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.budgetEntry != null) {
+      _titleController.text = widget.budgetEntry!.title!;
+      _descriptionController.text = widget.budgetEntry!.description ?? '';
+      _amountController.text = widget.budgetEntry!.amount?.toString() ?? '';
+    }
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descriptionController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  void navigateToHomepage(BuildContext context) {
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => const Homepage()),
+      (route) => false,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final BudgetEntry? budgetEntry = widget.budgetEntry;
+
+    final currentContext = context;
 
     bool isCreateFlag = budgetEntry == null ? true : false;
 
@@ -25,7 +61,98 @@ class _CreatebudgetentryState extends State<Createbudgetentry> {
       appBar: AppBar(
         title: Text(titleText),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                controller: _titleController,
+                decoration: const InputDecoration(
+                  labelText: 'Title',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a title';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _descriptionController,
+                decoration: const InputDecoration(
+                  labelText: 'Description (optional)',
+                ),
+              ),
+              TextFormField(
+                controller: _amountController,
+                decoration: const InputDecoration(
+                  labelText: 'Amount',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an amount';
+                  }
+                  final amount = double.tryParse(value);
+                  if (amount == null || amount <= 0) {
+                    return 'Please enter a valid amount';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    // Form is valid, submit the data
+                    final title = _titleController.text;
+                    final description = _descriptionController.text;
+                    final amount = double.parse(_amountController.text);
+
+                    if (isCreateFlag) {
+                      // Create a new budget entry
+                      final newEntry = BudgetEntry(
+                        title: title,
+                        description:
+                            description.isNotEmpty ? description : null,
+                        amount: amount,
+                      );
+                      // TODO: Save the new entry to the database
+                      final request = ModelMutations.create(newEntry);
+                      final response =
+                          await Amplify.API.mutate(request: request).response;
+                      final createdBudgetEntry = response.data;
+                      if (createdBudgetEntry == null) {
+                        safePrint('errors: ${response.errors}');
+                        return;
+                      }
+                      safePrint('Mutation result: ${createdBudgetEntry.title}');
+                      if (!mounted) return;
+                      navigateToHomepage(currentContext);
+                    } else {
+                      //update budgetEntry instead
+                      final updateBudgetEntry = budgetEntry.copyWith(
+                          title: title,
+                          description:
+                              description.isNotEmpty ? description : null,
+                          amount: amount);
+                      final request = ModelMutations.update(updateBudgetEntry);
+                      final response =
+                          await Amplify.API.mutate(request: request).response;
+                      safePrint('Response: $response');
+                      if (!mounted) return;
+                      navigateToHomepage(currentContext);
+                    }
+                  }
+                },
+                child: Text(titleText),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
